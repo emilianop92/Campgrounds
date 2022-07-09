b8166b1005af912d71305dc3de0994f983c7b0c8
Emiliano Pineda <emilianop92@gmail.com>
2022-07-08 20:39:51 -0600

    progress on seed data

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const cities = require('./cities')
+
+// Module for creating a campground object with a method for saving to mongodb
+const Campground = require('../models/campground')
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/campgrounds')
+    .then(()=>{
+        console.log("Connection Open")
+    })
+    .catch(()=>{
+        console.log("ERROR")
+        console.log(err)
+    })
+
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    for(let i =0; i<50; i++) {
+        const rand1000 = Math.floor(Math.random*1000)
+    }
+    const c = new Campground
+    await c.save()
+}
+
+seedDB()
