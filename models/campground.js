962ccf9a417cb1397a61607480c4318c7c13e87b
Emiliano Pineda <emilianop92@gmail.com>
2022-07-13 19:15:36 -0600

    annotation

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-const campgroundSchema = new Schema({
+const campgroundSchema = new mongoose.Schema({
     title: String,
     price: String,
     description: String,
     location: String
 })
 
+// Allows the use of the schema when called by another file
 module.exports = mongoose.model('Campground', campgroundSchema)
