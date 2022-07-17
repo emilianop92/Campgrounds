a5e12ae1aed2e38e968515452b01f1a232919fd8
Emiliano Pineda <emilianop92@gmail.com>
2022-07-17 17:43:43 -0600

    Added ejsMate engine

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 
@@ -16,9 +17,12 @@ mongoose.connect('mongodb://localhost:27017/campgrounds')
         console.log(err)
     })
 
-// Express Middleware
+// Configure Express
+app.engine('ejs', ejsMate)  // For EJS files, use the ejsMate engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+// Express Middleware
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
