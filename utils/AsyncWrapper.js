ddfc3f1ea7522b4bfcfc3b4bcf905cda86f036dd
Emiliano Pineda <emilianop92@gmail.com>
2022-08-19 07:51:24 -0600

    asyncWrapper documentation

--- utils/AsyncWrapper.js
+++ utils/AsyncWrapper.js
@@ -1,3 +1,5 @@
+// Wraps async functions to automatically pass in next.
+// If there is an error, it will pass in the error to direct to error handling
 function AsyncWrapper(fn) {
     return function(req, res, next){
         fn(req, res, next).catch(e => next(e))
